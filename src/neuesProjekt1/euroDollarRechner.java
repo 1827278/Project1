1335e238b51aa275a83e60fcf4e74f3c3bd25e4f
alexa <alexa@141.19.145.45>
2018-10-17 09:23:54 +0200

    Final

--- src/neuesProjekt1/euroDollarRechner.java
+++ src/neuesProjekt1/euroDollarRechner.java
@@ -2,31 +2,33 @@ package neuesProjekt1;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class euroDollarRechner {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		// declaration of unknown
 		Scanner scan = new Scanner(System.in);
-		int Währung;
-		String Währung1;
+		int currency;
+		String currency1;
 		float euro;
 		String euro1;
 		float dollar;
 		String dollar1;
 		boolean again;
 		String x;
+		int i = 0;
 		
 		
 		do {
 			
 			// decide Euro or Dollar
 			System.out.println("Wählen Sie 1 für Euro in Dollar oder 2 für Dollar in Euro aus:");
-			Währung1 = scan.nextLine();
-			Währung = Integer.parseInt(Währung1);
+			currency1 = scan.nextLine();
+			currency = Integer.parseInt(currency1);
 
 			// If Euro
-			if (Währung == 1) {
+			if (currency == 1) {
 				System.out.println("Geben Sie einen Betrag in Euro ein:");
 				euro1 = scan.nextLine();
 				euro = Float.parseFloat(euro1);
@@ -47,7 +49,7 @@ public class euroDollarRechner {
 				}
 			
 			// If Dollar
-			else if (Währung == 2) {
+			else if (currency == 2) {
 				System.out.println("Geben Sie einen Betrag in Dollar ein:");
 				dollar1 = scan.nextLine();
 				dollar = Float.parseFloat(dollar1);
@@ -61,6 +63,7 @@ public class euroDollarRechner {
 				else if (x.equals("N"))
 				{
 					again=false;
+					break;
 				}
 				else {
 					again=false;
@@ -68,8 +71,25 @@ public class euroDollarRechner {
 			}
 			// Else Error
 			else {
-				System.out.println("Error");
+				System.out.println("Error\nBitte Warten...");
+				System.out.println("Weitere Umrechnung benötigt?");
+				x = scan.nextLine();
+				if (x.equals("J"))
+						{
+					again=true;
+						}
+				else if (x.equals("N"))
+				{
+					again=false;
+					break;
+				}
+				else {
+					again=false;
+				}
+
+				
 				 }
+			
 		}while(again=true);
 	
 	
